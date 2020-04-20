7cec1c4a87b153f174d6314f92c17f9f43a936cd
miracle928 <justincasul@gmail.com>
2020-04-20 20:49:41 +0800

    first commit

---
+++ register.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Social Network  | Odiary 2020</title>
+        <link rel="shortcut icon" type="icon" href="connecter.png">
+        <meta name="description" content="#">
+        <meta name="keywords" content="#">
+        <meta name="author" content="#">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link href="https://fonts.googleapis.com/css2?family=Ropa+Sans&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Julius+Sans+One&family=Ropa+Sans&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Julius+Sans+One&family=Ropa+Sans&family=Sen&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        
+    </body>
+    </html>
